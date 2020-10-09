c0d24bfd418ca761a7de90ba5781269c3a2d6296
kyle <kwarchuck@gmail.com>
2020-10-09 10:50:24 -0400

    made contact object working on contact page

---
+++ Data/Contact.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class ContactObj
+{
+    public Guid Id { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime BirthDate { get; set; }
+
+}
