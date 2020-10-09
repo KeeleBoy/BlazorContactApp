6c31288db8c2bdca6a3757b26fa6e9ee374d07bc
kyle <kwarchuck@gmail.com>
2020-10-09 13:33:35 -0400

    cleaned up some code

--- Data/Contact.cs
+++ Data/Contact.cs
@@ -1,14 +1,14 @@
 using System;
 
-public class ContactObj
+public class contactObj
 {
-    public Guid Id { get; set; }
-    public string LastName { get; set; }
-    public string FirstName { get; set; }
-    public string PhoneNumber { get; set; }
-    public DateTime BirthDate { get; set; }
+    public Guid id { get; set; }
+    public string lastName { get; set; }
+    public string firstName { get; set; }
+    public string phoneNumber { get; set; }
+    public DateTime birthDate { get; set; }
 
 }
 
-//var ContactList = new List<ContactObj> {};
+//var contactList = new List<contactObj> {};
 // attempted to make list of contact object here and failed
