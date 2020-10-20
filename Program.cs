a73b6d8cbc4c77c160041d2f278d3968848614f1
Sean <s.warchuck16@gmail.com>
2020-10-19 22:11:12 -0400

    a weekend worth of work, almost have it running

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using BlazorContactApp.Data;
 
 namespace ContactSystem
 {
@@ -18,15 +19,15 @@ namespace ContactSystem
             CreateHostBuilder(args).Build().Run();
 
             // here is an attempt to create a list of the contact object
-            List<contactObj> contacts = new List<contactObj>;
+            List<Contact> contacts = new List<Contact>();
 
             // here i'm trying to add a contact so we can see if the list is working properly
-            contacts.Add(new contactObj);
-            contacts[0].FirstName = "Jeff";
-            contacts[0].LastName = "Guy";
-            contacts[0].PhoneNumber = "123-456-7890";
-            contacts[0].BirthDate = "0:01-01-1999";
-            contacts[0].Id = "1";
+            //contacts.Add(new Contact)();
+            //contacts[0].FirstName = "Jeff";
+            //contacts[0].LastName = "Guy";
+            //contacts[0].PhoneNumber = "123-456-7890";
+            //contacts[0].BirthDate = "0:01-01-1999";
+            //contacts[0].Id = "1";
 
             foreach (contactObj contact in contacts)
                 {
